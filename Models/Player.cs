a6fca7ca62d2754dfa18877ab9f2f90e08d6a953
jonasvogel <jonasvogel@JONASVGELB7A9>
2024-05-02 14:26:13 +0200

    Add project files.

---
+++ Models/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace Models;
+
+public class Player
+{
+    public int PlayerId { get; set; }
+    
+    public string FName { get; set; }
+    
+    public string LName { get; set; }
+    
+    public string FullName { get { return $"{FName} {LName}"; } }
+    
+    public DateTime BirthDate { get; set; }
+    
+    public DateTime Entry { get; set; }
+    
+    public DateTime Exit { get; set; }
+    
+    public string Notes { get; set; }
+    
+    
+}
