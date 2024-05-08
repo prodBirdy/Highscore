54e4d62377134cf00d76dfa9f6dd541c2afedc70
impscorp <jonvoegel@tsn.at>
2024-05-08 10:14:43 +0200

    Test

--- Models/Highscore.cs
+++ Models/Highscore.cs
@@ -2,21 +2,7 @@
 
 public class Highscore
 {
-    public int PlayerId { get; set; }
-    
-    public string FName { get; set; }
-    
-    public string LName { get; set; }
-    
-    public string FullName { get { return $"{FName} {LName}"; } }
-    
-    public DateTime BirthDate { get; set; }
-    
-    public DateTime Entry { get; set; }
-    
-    public DateTime Exit { get; set; }
-    
-    public string Notes { get; set; }
+   
     
     
 }
--- Models/Player.cs
+++ Models/Player.cs
@@ -6,7 +6,7 @@ public class Player
     
     public string FName { get; set; }
     
-    public string LName { get; set; }
+    public string? LName { get; set; }
     
     public string FullName { get { return $"{FName} {LName}"; } }
     
